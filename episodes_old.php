9009778612745d535fe0d77c3c1f2ed91bb53a8f
celebrater <tkgm45nbchk@gmail.com>
2022-01-19 19:24:22 +0900

    cleaned up index

--- episodes_old.php
+++ episodes_old.php
@@ -1,10 +1,44 @@
 <?php
 
-require_once "Novel.php";
+require_once "get_chapters_and_episodes.php";
+require_once "get_title_and_captions.php";
 
-$id = (int)$_GET["id"];
-$list = file("novels/novels_list.txt"); // 第三世界収容所|prison, 白金記|shiroganeki, 極楽戦争|gokuraku
-$novel = new Novel($list[$id]);
+$title_and_path = get_title_and_path(); // [["第三世界収容所", "prison"], ["白金記", "shiroganeki"]... ]
+$captions = get_captions($title_and_path);
+
+$title = $title_and_path[$_GET['novel_id']][0];
+$path = "novels/" . $title_and_path[$_GET['novel_id']][1];
+$caption = $captions[$_GET['novel_id']];
+$list = file($path . "/list.txt"); // ["1|001|第一話", "1|2|第二話", "1|03|第三話"...]
+
+$chapters_and_episodes = [];
+$only_path_eps = [];
+$temp = [];
+
+$file_names = get_file_names($path, $list); // ["novels/shiroganeki/001.txt", "novels/shiroganeki/2.txt"...]
+
+//$temp_txt_files = glob($path . "/txts/*.txt");
+//
+//foreach ($temp_txt_files as $file){
+//    // "novels/shiroganeki/txts/1.txt" => "1"
+//    $temp = str_replace($path . "/txts/", "", $file);
+//    array_push($txt_files, $temp);
+//}
+//$txt_files = "hello World";
+
+//$test_get_episodes_and_chapters= get_episodes_and_chapters("shiroganeki");
+$has_chapters = has_chapters($path);
+if($has_chapters){
+    // ["chapter" => "第一章「日本編", "path_eps" => [[[path, ep], [path, ep]], [[path, ep], [path, ep]]...] ]
+    $chapters_and_episodes = get_chapters_and_episodes($path);
+} else {
+    if(file_exists($path . "/list.txt")){
+//        $temp = file($path . "/list.txt"); // ["1|第一話", "1|第二話", "1|第三話", "2|第四話"...]
+        $only_path_eps = get_path_eps($path, $list); // [["~/001.txt", "第一話"], ["~/2.txt", "第二話"], ["~/03.txt", "第三話"]...]
+    } else {
+        $only_path_eps = ["チャプターリスト（list.txt）が存在しないか、読み込めません。list.txt does not exist or unavailable."];
+    }
+}
 
 ?>
 
--- index.php
+++ index.php
@@ -1,21 +1,13 @@
 <?php
 
-//require_once "get_chapters_and_episodes.php";
-//require_once "get_title_and_captions.php";
 require_once "Novel.php";
 
 $novels_list = file("novels/novels_list.txt"); // 第三世界収容所|prison, 白金記|shiroganeki, 極楽戦争|gokuraku
 $novels = [];
-//$shiroganeki = new Novel("白金記", "shiroganeki");
 
 foreach ($novels_list as $novel){
     array_push($novels, new Novel($novel));
 }
-
-// Test to create the array from List of TXT
-
-//$title_and_path = get_title_and_path();
-//$captions = get_captions($title_and_path);
 
 ?>
 
@@ -25,20 +17,20 @@ foreach ($novels_list as $novel){
     <meta charset="utf-8">
     <meta name="Author" content="Enin Fujimi">
     <link rel="stylesheet" href="css/style.css" type="text/css">
-    <title>Test Novel List</title>
+    <title>Personal Novel Server</title>
 </head>
 <body>
     <div class="containter">
         <h1>
             <a href="/">
-                Test Novel List
+                Personal Novel Server
             </a>
         </h1>
-        <p>Memo: ["episode" => "第一章「日本編", "chapters" => ["第一話", 第二話...]], [</p>
+
         <?php for ($i = 0; $i < count($novels_list); $i++) : ?>
             <hr>
             <h2>
-                <a href="show_episodes.php?novel_id=<?php echo 1; ?>">
+                <a href="show_episodes.php?novel_id=<?php echo $i; ?>">
                     <?php echo $novels[$i]->title; ?>
                 </a>
             </h2>
