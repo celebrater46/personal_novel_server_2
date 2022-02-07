e4e2a1cde56b3bcb2548452cf376ccebdd5604a3
celebrater <tkgm45nbchk@gmail.com>
2022-02-07 14:50:16 +0900

    cleaned up main.js

--- js/main.js
+++ js/main.js
@@ -10,24 +10,7 @@ const color_options = document.querySelectorAll("#color option");
 const xy = document.querySelector("#xy");
 const xy_options = document.querySelectorAll("#xy option");
 
-// font_family.addEventListener('change', () =>{
-//     // let index =  this.selectedIndex;
-//     console.log(font_family.selectedIndex);
-//     body.style.fontFamily = "Sawarabi " + (font_family.selectedIndex === 0 ? "Gothic" : "Mincho");
-//     // setTimeout( () => console.log(this.selectedIndex) ,1000 );
-// });
-
-// const select = document.querySelector('[name="member"]');
-//
 font_family.onchange = event => {
     console.log(font_family.selectedIndex);
     body.style.fontFamily = "Sawarabi " + (font_family.selectedIndex === 0 ? "Gothic" : "Mincho");
 }
-
-
-// console.log(font_family);
-// body.style.fontFamily = "ＭＳ 明朝";
-
-// setTimeout( () => console.log(3) ,1000 );
-// setTimeout( () => console.log(2) ,2000 );
-// setTimeout( () => console.log(1) ,3000 );
