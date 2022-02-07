2458252fe90180e178ccec429e51777f0e17327a
celebrater <tkgm45nbchk@gmail.com>
2022-02-07 14:49:32 +0900

    addListener problem has been resolved

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const body = document.querySelector("body");
-const font_family = document.querySelector("#font_family");
+const font_family = document.querySelector('[name="font_family"]');
 const font_family_options = document.querySelectorAll("#font_family option");
 const font_size = document.querySelector("#font_size");
 const font_size_options = document.querySelectorAll("#font_size option");
@@ -10,15 +10,22 @@ const color_options = document.querySelectorAll("#color option");
 const xy = document.querySelector("#xy");
 const xy_options = document.querySelectorAll("#xy option");
 
-font_family.addEventListener('change', () =>{
-    // let index =  this.selectedIndex;
-    body.style.fontFamily = "Sawarabi " + (this.selectedIndex === 0 ? "Gothic" : "Mincho");
-    console.log(this.selectedIndex);
-    setTimeout( () => console.log(this.selectedIndex) ,1000 );
-});
+// font_family.addEventListener('change', () =>{
+//     // let index =  this.selectedIndex;
+//     console.log(font_family.selectedIndex);
+//     body.style.fontFamily = "Sawarabi " + (font_family.selectedIndex === 0 ? "Gothic" : "Mincho");
+//     // setTimeout( () => console.log(this.selectedIndex) ,1000 );
+// });
+
+// const select = document.querySelector('[name="member"]');
+//
+font_family.onchange = event => {
+    console.log(font_family.selectedIndex);
+    body.style.fontFamily = "Sawarabi " + (font_family.selectedIndex === 0 ? "Gothic" : "Mincho");
+}
 
 
-console.log(font_family);
+// console.log(font_family);
 // body.style.fontFamily = "ＭＳ 明朝";
 
 // setTimeout( () => console.log(3) ,1000 );
