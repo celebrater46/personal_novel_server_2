6afa0ffbf5d9f212e71c0f5cc66eb8aa9d3ef8ca
celebrater <tkgm45nbchk@gmail.com>
2022-03-23 02:12:49 +0900

    edited init and pns_js_init

--- init.php
+++ init.php
@@ -3,18 +3,17 @@
 namespace personal_novel_server;
 
 ini_set('display_errors', 1); // エラーメッセージを常時表示する
-//define("PNS_SITE_NAME", 'Personal Novel Server');
-define("PNS_SITE_NAME", '小説');
+define("PNS_SITE_NAME", 'Personal Novel Server');
+//define("PNS_SITE_NAME", '小説');
 define("PNS_DESCRIPTION", '富士見永人名義で小説を書いています。<br><a href="https://novelup.plus/user/350590019/profile">ノベルアッププラス</a>、 <a href="https://mypage.syosetu.com/476781/">小説家になろう</a>、 <a href="https://kakuyomu.jp/users/eningrad">カクヨム</a>で活動中です。');
 define("PNS_AUTHOR", 'Enin Fujimi');
-//define("PNS_INDEX_FILE", 'http://localhost/myapps/personal_novel_server_2/index.php'); // 小説一覧ページを別途用意する場合は、ここを書き換え
-//define("PNS_INDEX_FILE", 'http://localhost/myapps/fujimipolis/files/novel.php'); // 小説一覧ページを別途用意する場合は、ここを書き換え
-define("PNS_INDEX_FILE", 'https://enin-world.sakura.ne.jp/files/novel.php'); // 小説一覧ページを別途用意する場合は、ここを書き換え
-//define("PNS_INDEX_FILE_NAME", 'novel.php'); // 小説一覧ページを別途用意する場合は、ここを書き換え（こちらはディレクトリなし）
+define("PNS_INDEX_FILE", ''); // 小説一覧ページを別途用意する場合は、ここを書き換え
+//define("PNS_INDEX_FILE", 'http://localhost/myapps/personal_novel_server_2/index.php');
+//define("PNS_INDEX_FILE", 'http://localhost/myapps/fujimipolis/files/novel.php');
+//define("PNS_INDEX_FILE", 'https://enin-world.sakura.ne.jp/files/novel.php');
 define('PNS_NOVELS_DIR', __DIR__ . '/novels'); // 小説のディレクトリ（__DIR__ は現在のディレクトリ取得）
-//define('PNS_PATH', ''); // プロジェクトフォルダのパス
-define('PNS_PATH', 'app/php/personal_novel_server/'); // プロジェクトフォルダのパス
+define('PNS_PATH', ''); // プロジェクトフォルダのパス（外部サイトに組み込む場合は書き換える）
+//define('PNS_PATH', 'app/php/personal_novel_server/');
 define('IMAGES_DIR_HTTP', 'img'); // HTTPでアクセスした際にディレクトリが変わるので用意
-//define('USE_GET_FUNCTION', true); // 外部サイト組込用の関数を使用するか（true で使用）
-//define('PNS_LIGHT_AND_DARK', false); // 個人サイト用
-define('PNS_LIGHT_AND_DARK', true); // 個人サイト用
+define('PNS_LIGHT_AND_DARK', false); // 個人サイト用
+//define('PNS_LIGHT_AND_DARK', true);
--- js/pns_js_init.js
+++ js/pns_js_init.js
@@ -1,7 +1,7 @@
 let PNS = {};
 
-// PNS.pnsPath = "";
-PNS.pnsPath = "app/php/personal_novel_server/";
-// PNS.indexFileName = "index.php"; // 別途ページを用意する場合、ここを書き換える
-PNS.indexFileName = "novel.php"; // 別途ページを用意する場合、ここを書き換える
+PNS.pnsPath = ""; // index.php を別途用意する場合（外部サイトへの組み込み等）、プロジェクトフォルダのディレクトリを書く
+// PNS.pnsPath = "app/php/personal_novel_server/";
+PNS.indexFileName = "index.php"; // index.php を別途用意する場合（外部サイトへの組み込み等）、index.php に相当するファイル名に書き換える
+// PNS.indexFileName = "novel.php";
 PNS.vFileName = PNS.pnsPath + "v.php";
